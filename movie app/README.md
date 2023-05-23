0a569a61c6d7c303e5d11064902c210170798fdc
Laura <lauracolof@gmail.com>
2023-05-23 17:00:09 -0300

    avoid the same search

--- movie app/README.md
+++ movie app/README.md
@@ -14,11 +14,11 @@ Requerimientos:
 
 - [x] Haz que las películas se muestren en un grid responsive.
 
-- [] Hacer el fetching de datos a la API
+- [x] Hacer el fetching de datos a la API
 
 Primera iteración:
 
-- [] Evitar que se haga la misma búsqueda dos veces seguidas.
+- [x] Evitar que se haga la misma búsqueda dos veces seguidas.
 
 - [] Haz que la búsqueda se haga automáticamente al escribir.
 
--- movie app/src/hooks/UseMovies.jsx
+++ movie app/src/hooks/UseMovies.jsx
@@ -1,13 +1,28 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { searchMovies } from '../services/movies.js';
 
+//black box to iterate the contract to fetching the data
 export function useMovies({ search }) {
   const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [, setError] = useState(null);
+  const previousSearch = useRef(search);
 
   const getMovies = async () => {
-    const newMovies = await searchMovies({ search });
-    setMovies(newMovies);
+    if (search === previousSearch.current) return;
+
+    try {
+      setLoading(true);
+      setError(null);
+      previousSearch.current = search;
+      const newMovies = await searchMovies({ search });
+      setMovies(newMovies);
+    } catch (error) {
+      setError(e.message);
+    } finally {
+      setLoading(false);
+    }
   };
 
-  return { movies, getMovies };
+  return { movies, getMovies, loading };
 }
